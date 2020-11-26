5f2759a963581c483e04c2f386e7a460ac51d11f
BoHongtao@yeah.net <BoHongtao@yeah.net>
2020-11-26 15:55:26 +0800

    fix

---
+++ src/main/java/com/hulu/shop/api/controller/user/req/SignupParams.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.hulu.shop.api.controller.user.req;
+
+import javax.validation.constraints.NotNull;
+
+public class SignupParams {
+    @NotNull
+    public String phone;
+    @NotNull
+    public String passWord;
+    @NotNull
+    public String channel;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+}
